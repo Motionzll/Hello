021ad15fd9bc2c38d1e7a5ae83856ca5b120a0a3
Motionzll <59975780+Motionzll@users.noreply.github.com>
2020-01-16 14:10:34 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (void){
 printf("Ahmed Suliman Al Rawahi");
-printf("Mechanical Engineering");
+printf("Mechanical Engineering.");
  printf("Asama ALQuwaitai");
  printf("Mechanical Engineering");
 return 0;
